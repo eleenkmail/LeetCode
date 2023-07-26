a0d3e196d391620c3deff239f7efcf7f1ad9272b
Eleen Kmail <79339710+eleenkmail@users.noreply.github.com>
2023-07-26 14:22:32 +0300

    Time: 23 ms (51.61%) | Memory: 13.1 MB (91.08%) - LeetSync

---
+++ 202-happy-number/happy-number.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def isHappy(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        seen = {}
+        seen[n] = 1
+        
+        while(True):
+            summ = 0
+            while n != 0:
+                summ += (n%10)**2
+                n = n/10
+
+            n = summ
+
+            print(n)
+
+            if n == 1:
+                return True
+
+            if seen.get(n, 0) + 1 > 1:
+                return False
+
+            seen[n] = seen.get(n, 0) + 1
+
+
+        
