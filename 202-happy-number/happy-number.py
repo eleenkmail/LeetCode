f1e06d5b335e055a09d82512c180ab78464dbe5d
Eleen Kmail <79339710+eleenkmail@users.noreply.github.com>
2023-07-26 14:40:11 +0300

    Time: 15 ms (91.61%) | Memory: 13.3 MB (29.07%) - LeetSync

--- 202-happy-number/happy-number.py
+++ 202-happy-number/happy-number.py
@@ -6,23 +6,31 @@ class Solution(object):
         """
         seen = {}
         seen[n] = 1
+        summ = 0
         
         while(True):
-            summ = 0
-            while n != 0:
-                summ += (n%10)**2
-                n = n/10
 
-            n = summ
+            summ += (n%10)**2
+            n = n/10
+
+            if n == 0:
+                n = summ
+                summ = 0
+                if n == 1:
+                    return True
+
+                if seen.get(n, 0) + 1 > 1:
+                    return False
+
+                seen[n] = seen.get(n, 0) + 1
 
 
-            if n == 1:
-                return True
 
-            if seen.get(n, 0) + 1 > 1:
-                return False
+            
 
-            seen[n] = seen.get(n, 0) + 1
+            
+
+            
 
 
         
